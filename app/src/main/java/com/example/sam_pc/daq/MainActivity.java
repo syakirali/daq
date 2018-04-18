6bc20a7b99b99f3da923d90cf076b83d8df48340
Syakir Ali <email02081997@gmail.com>
2018-04-18 20:09:51 +0700

    create project

---
+++ app/src/main/java/com/example/sam_pc/daq/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sam_pc.daq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
